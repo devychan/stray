13f31ce4327ab7a3a6468d86d53cd1e5a3947171
devychan <devychan>
2025-01-20 06:01:07 +0800

    Updated port

--- app.py
+++ app.py
@@ -69,4 +69,5 @@ def predict_img():
         "message" : "File is not allowed by its mime."
       })
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 4000))  # Default to 4000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
